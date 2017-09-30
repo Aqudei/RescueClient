4b3066a48f93cb8bb12ef57cbc3e0c2d7ca505ab
Aqudei <yodi_cute07@yahoo.com>
2017-09-30 14:42:59 +0800

    aa

---
+++ RescueApp/WPF/BoolToValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace RescueApp.WPF
+{
+    public class BoolToStringConverter : BoolToValueConverter<String> { }
+    public class BoolToBrushConverter : BoolToValueConverter<Brush> { }
+    public class BoolToVisibilityConverter : BoolToValueConverter<Visibility> { }
+    public class BoolToObjectConverter : BoolToValueConverter<Object> { }
+
+    public class BoolToValueConverter<T> : IValueConverter
+    {
+        public T FalseValue { get; set; }
+        public T TrueValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return FalseValue;
+            else
+                return (bool)value ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value != null ? value.Equals(TrueValue) : false;
+        }
+    }
+}
