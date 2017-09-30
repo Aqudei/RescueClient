5663eaa449c3509c6f76efd8e0cd495dbc2e56aa
Aqudei <yodi_cute07@yahoo.com>
2017-09-30 09:46:37 +0800

    sss

--- RescueApp/Views/AddEditHouseholdVM.cs
+++ RescueApp/Views/AddEditHouseholdVM.cs
@@ -1,5 +1,6 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
+using MahApps.Metro.Controls.Dialogs;
 using RescueApp.Interfaces;
 using RescueApp.Messages;
 using RescueApp.Models;
@@ -20,6 +21,7 @@ namespace RescueApp.Views
         IEditorDialog<DownloadHouseholdModel>
     {
         private readonly DialogService dialogService;
+        private readonly IDialogCoordinator dialogCoordinator;
         private readonly RescueClient rescueClient;
 
         public DownloadHouseholdModel Current { get; set; } = new DownloadHouseholdModel();
@@ -37,10 +39,12 @@ namespace RescueApp.Views
             AutoMapper.Mapper.Map(item, Current, typeof(DownloadHouseholdModel), typeof(DownloadHouseholdModel));
         }
 
-        public AddEditHouseholdVM(RescueClient rescueClient, DialogService dialogService)
+        public AddEditHouseholdVM(RescueClient rescueClient, DialogService dialogService,
+            IDialogCoordinator dialogCoordinator)
         {
             this.rescueClient = rescueClient;
             this.dialogService = dialogService;
+            this.dialogCoordinator = dialogCoordinator;
         }
 
         public override void OnShow<T>(T args)
@@ -75,6 +79,11 @@ namespace RescueApp.Views
                             if (ex == null)
                             {
                                 ClearFields();
+                                dialogCoordinator.ShowMessageAsync(this, "Success", "HOUSEHOLD INFO CHANGED");
+                            }
+                            else
+                            {
+                                dialogCoordinator.ShowMessageAsync(this, "FAILURE", "FAILED TO CHANG HOUSEHOLD INFO");
                             }
                             MessengerInstance.Send(new AddEditResultMessage<DownloadHouseholdModel>(ex, hs));
                         }, ChoosenPhoto);
